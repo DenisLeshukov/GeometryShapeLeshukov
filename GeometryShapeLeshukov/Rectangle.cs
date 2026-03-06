de556de81c7a43a9ece2dbcc8eda99b1ab7cd445
DenisLeshukov <leshukov80@list.ru>
2026-03-06 13:39:16 +0500

    commit

---
+++ GeometryShapeLeshukov/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle:Shape
+    {
+        private double Width { get; set; }
+        private double Height { get; set; }
+        public Rectangle (double _wight, double _height)
+        {
+            Height = _height;
+            Width = _wight;
+        }
+        public override double Area()
+        {
+            if(Width > 0 && Height > 0)
+            {
+                return Width * Height;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Неверные данные (стороны должжны быть > 0)");
+            }
+            
+        }
+        public override double Perimeter()
+        {
+            if (Width > 0 && Height > 0)
+            {
+                return (Width + Height) * 2;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Неверные данные (стороны должжны быть > 0)");
+            }
+           
+        }
+        public override string ToString()
+        {
+            return $"Прямоугольник: высота={Height}, ширина={Width}";
+        }
+    }
+}
---
+++ GeometryShapeLeshukov/Triangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Triangle:Shape
+    {
+        private double SideA { get; set; } 
+        private double SideB { get; set; }
+        private double SideC { get; set; }
+        public Triangle (double _sideA,double _sideB, double _sideC)
+        {
+
+            SideA = _sideA;
+            SideB = _sideB;
+            SideC = _sideC;
+ 
+        }
+        public override double Area ()
+        {
+            if (SideA + SideB > SideC)
+            {
+                if (SideA > 0 && SideB > 0 && SideC > 0)
+                {
+                    double p = (SideA + SideB + SideC) / 2;
+                    return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+                }
+                else
+                { 
+                    throw new ArgumentOutOfRangeException("Неверные данные (стороны должжны быть > 0)");
+                }
+
+            }
+            return -1;
+        }
+        public override double Perimeter ()
+        {
+            if(SideA+SideB>SideC)
+            {
+                if (SideA <= 0 || SideB <= 0 || SideC <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Неверные данные (стороны должжны быть > 0)");
+                }
+                else
+                {
+                    return SideA + SideB + SideC;
+                }
+                
+            }
+            return 0;
+            
+        }
+        public override string ToString ()
+        {
+            return $"Треугольник: сторона а={SideA}, сторона b= {SideB}, сторона с={SideC}";
+        }
+    }
+}
